d6e338a527556d0a3db3af352d1e9c8a716ec972
eoussama <oussama.essamadi@gmail.com>
2025-01-13 00:56:24 +0100

    refact: assets

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,6 +8,7 @@ name="datascience-notebook-container"
 docker run -it --rm \
   -p 8888:8888 \
   --name "$name" \
-  -v "$(pwd)/src:/home/jovyan/work" \
+  -v "$(pwd)/src:/home/jovyan/work/src" \
+  -v "$(pwd)/assets:/home/jovyan/work/assets" \
   "$image" \
   start-notebook.sh --NotebookApp.token=''
